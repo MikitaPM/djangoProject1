e44d45f3133af6e3b8ba31137bf751db22ed22ed
MikitaPM <de4lt3774@gmail.com>
2023-01-12 21:59:45 +0300

    2

--- main/views.py
+++ main/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 
 
+posts = [
+    {
+        'author': 'Admin',
+        'title': 'First post',
+        'content': 'Sodejanie',
+        'date_posted': '12/01/2023'
+    },
+    {
+'author': 'User1',
+        'title': 'Second post',
+        'content': 'Sodejanie',
+        'date_posted': '12/01/2023'
+    }
+]
+
+
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'posts':posts
+    }
+    return render(request, 'main/home.html', context)
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'main/about.html', {'title': '0 clube Python'})
 
 # Create your views here.
